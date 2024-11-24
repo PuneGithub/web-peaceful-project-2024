8b39b0e96c89acb990e436fc71bba8f602fede85
Pune <minecraftpune@gmail.com>
2024-11-24 16:08:36 +0700

    Add file account.php

---
+++ account/account.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+require_once '../system/conn.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/output.css">
+    <title>Peaceful Network</title>
+</head>
+<script src="js/script.js"></script>
+
+<body style="background-image: url('../img/bg.webp');">
+
+    <?php include_once("../components/header-navbar.php"); ?>
+
+    <!-- header navbar -->
+    <div class="flex items-center justify-center h-screen">
+        <div class="card-white w-full max-w-md">
+            <h2 class="text-xl font-semibold text-center">Account</h2>
+            <?php
+
+            ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
--- account/login.php
+++ account/login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once '../system/conn.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
