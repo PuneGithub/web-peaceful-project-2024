fce8be4f11a5307d65c73d217d213c82403424da
minecraftpune@gmail.com <53265897+PuneGithub@users.noreply.github.com>
2025-03-18 18:02:26 +0700

    Update Database

--- peaceful_network.sql
+++ peaceful_network.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 01, 2025 at 01:24 PM
+-- Generation Time: Mar 18, 2025 at 11:01 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -46,6 +46,18 @@ INSERT INTO `category` (`categoryId`, `categoryName`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `lovelogs`
+--
+
+CREATE TABLE `lovelogs` (
+  `loveId` int NOT NULL,
+  `userId` int NOT NULL,
+  `postId` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `posts`
 --
 
@@ -65,8 +77,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`postId`, `userId`, `title`, `content`, `createdAt`, `categoryId`, `loveCount`, `imagePost`) VALUES
-(32, 27, 'test', 'test', '2025-01-28 14:08:31', 1, 0, NULL),
-(33, 27, 'awdawdawd', 'sasdwadawdawdawda&lt;br&gt;\r\nawdawdawdawd', '2025-02-01 09:48:35', 2, 0, 'post_17384033157c8ae7bf-71d5-49e2-9c16-3bcf82aadb5d.jpg');
+(39, 27, 'test123ฟฟรไฟกdawd', 'dwadawdfhwdwadawd&lt;br&gt;\r\ndaw4d65asddwad', '2025-03-05 13:38:08', 1, 0, 'post_17411829287c8ae7bf-71d5-49e2-9c16-3bcf82aadb5d.jpg');
 
 -- --------------------------------------------------------
 
@@ -93,7 +104,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userId`, `username`, `password`, `email`, `profileImage`, `verifyEmail`, `resetCode`, `createDate`, `verifyStatus`, `status`, `role`) VALUES
-(27, 'test123', '$2y$10$P6s3We3rkqeMQZl9yEo.UO..qKEZQAHB1wRMk5uyCq0X.yNsxsLxW', 'dev.peaceful@gmail.com', NULL, NULL, NULL, '2025-01-28', 'verified', 'offline', 'user');
+(27, 'test123', '$2y$10$P6s3We3rkqeMQZl9yEo.UO..qKEZQAHB1wRMk5uyCq0X.yNsxsLxW', 'dev.peaceful@gmail.com', NULL, NULL, NULL, '2025-01-28', 'verified', 'offline', 'user'),
+(28, 'pune2024', '$2y$10$s9suTUqwdWrWkseR5p7hrOz1oAaUvVaqjQlAzMc7akzUhBZRs.6pC', 'minecraftpune@gmail.com', NULL, NULL, NULL, '2025-03-07', 'verified', 'offline', 'user'),
+(29, 'dwadawd', '$2y$10$VNWUUkDtcHiafOqYsxD6XO1f1d7uka1cZwttiUU8gyRyBFwnAUwQm', 'wjf1oawdaw@gmail.com', NULL, '7b99387da6ffad9c93ecc9e693a7b7a8bd30a5dd213a17dea80114f0dec39fb8a394937330f114e87c16e5631fb832a79332', NULL, '2025-03-12', 'unverified', 'offline', 'user'),
+(30, 'muhaha', '$2y$10$0Fw/bjdaZ.fqDHj/oEbG..hDcwYOZkh7Pbux4fsuPKAJ.WDydJttK', 'muhahahah@gmail.com', NULL, '38faaeb0bc9eef6a1db85087385ccb3769729beea459b1729bc565d1b0de272ba6fcc8728d428c582ba231462fb914c37e79', NULL, '2025-03-12', 'unverified', 'offline', 'user');
 
 --
 -- Indexes for dumped tables
@@ -104,6 +118,14 @@ INSERT INTO `users` (`userId`, `username`, `password`, `email`, `profileImage`, 
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`categoryId`);
+
+--
+-- Indexes for table `lovelogs`
+--
+ALTER TABLE `lovelogs`
+  ADD PRIMARY KEY (`loveId`),
+  ADD UNIQUE KEY `unique_love` (`userId`,`postId`),
+  ADD KEY `postId` (`postId`);
 
 --
 -- Indexes for table `posts`
@@ -130,20 +152,33 @@ ALTER TABLE `category`
   MODIFY `categoryId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `lovelogs`
+--
+ALTER TABLE `lovelogs`
+  MODIFY `loveId` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `postId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `postId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `userId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `lovelogs`
+--
+ALTER TABLE `lovelogs`
+  ADD CONSTRAINT `lovelogs_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE,
+  ADD CONSTRAINT `lovelogs_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `posts`
