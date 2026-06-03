9bab7097b9c0885f66057b7afdf7474fcbefcb1e
minecraftpune@gmail.com <53265897+PuneGithub@users.noreply.github.com>
2026-06-03 14:43:02 +0700

    Update Customization.php

--- administrator/customization.php
+++ administrator/customization.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../system/websiteSettingsSystem.php';
+require_once __DIR__ . '/../system/config.php';
 session_start();
 
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
